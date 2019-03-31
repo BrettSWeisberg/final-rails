4a925a69dec79286db71be0f0fca91ba49d24bf3
Brett Weisberg <weisberg.brett@gmail.com>
2019-03-31 10:15:40 -0400

    Controllers updated with json

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,4 +1,5 @@
 class ApartmentsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
     @apartments = Apartment.all
 
@@ -7,29 +8,36 @@ class ApartmentsController < ApplicationController
 
   def new
     @apartment = Apartment.new
+    render json: @apartment
   end
 
   def create
     @project = Apartment.create(apartment_params)
+    @project.save
+    render json: @apartment
   end
 
   def show
     @apartment = Apartment.find(params[:id])
+    render json: @apartment
   end
 
   def edit
     @apartment = Apartment.find(params[:id])
+    render json: @apartment
   end
 
   def update
       @apartment = Apartment.find(params[:id])
       @apartment.update(apartment_params)
       @apartment.save
+      render json: @apartment
   end
 
   def destroy
       @apartment = Apartment.find(params[:id])
       @apartment.destroy
+      render json: @apartment
   end
 
 private
--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,4 +1,5 @@
 class BuildingsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
     @buildings = Building.all
 
@@ -7,25 +8,36 @@ class BuildingsController < ApplicationController
 
   def new
      @building = Building.new
+     render json: @building
   end
 
   def create
       @building = Building.new(building_params)
       @building.save
+      render json: @building
   end
 
   def show
     @building = Building.find(params[:id])
+    render json: @building
   end
 
   def edit
       @building = Building.find(params[:id])
+      render json: @building
   end
 
   def update
       @building = Building.find(params[:id])
       @building.update(building_params)
       @building.save
+      render json: @building
+  end
+
+  def destroy
+    @building = Building.find(params[:id])
+    @building.destroy
+    render json: @building
   end
 
 private
